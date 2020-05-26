6eef0fda908927927d734718d57f0f5ef693b372
Hayri Durmaz <hayri.durmaz@tedu.edu.tr>
2020-05-26 15:38:55 +0300

    parsePerson eklendi

--- src/createModal.py
+++ src/createModal.py
@@ -1,11 +1,10 @@
 import numpy as np  # helps with the math
-import numpy as np  # helps with the math
 import pandas as pd
 
+# Randomly permute [0,N] and extract indices for each fold
 from src.NeuralNetwork import Network
 
 
-# Randomly permute [0,N] and extract indices for each fold
 def crossval_folds(N, n_folds, seed=1):
     np.random.seed(seed)
     idx_all_permute = np.random.permutation(N)
@@ -24,13 +23,11 @@ def parsePeople():
     df = pd.read_csv('data/foreveralone.csv', delimiter=",", dtype={target_name: str})
     target2idx = {target: idx for idx, target in enumerate(sorted(list(set(df[target_name].values))))}
     X = df.drop([target_name], axis=1).values
-    enums = []
     personVals = []
     for col in df:
         if col == 'job_title' or col == 'depressed' or col == 'time' or col == 'social_fear' or col == 'what_help_from_others' or col == 'attempt_suicide' or col == 'improve_yourself_how':
             continue
         enum = {target: idx for idx, target in enumerate(sorted(list(set(df[col].values))))}
-        enums.append(enum)
         personVals.append(np.vectorize(lambda x: enum[x])(df[col].values))
 
     personVals = np.array(personVals).T
@@ -41,7 +38,35 @@ def parsePeople():
         raise Exception("X.shape = {} and y.shape = {} are inconsistent!".format(X.shape, y.shape))
     if normalize:
         personVals = (personVals - personVals.mean(axis=0)) / personVals.std(axis=0)
-    return personVals, y, n_classes
+    return personVals, y, n_classes, df
+
+
+def parsePerson(person_str, df):
+    person_str = '9/4/2016 23:10:04,Male,Straight,28,"$40,000 to $49,999",White non-Hispanic,Normal weight,Yes,No,' \
+                 'Yes but I ' \
+                 'haven\'t, 3.0, Yes, Yes, "wingman/wingwoman, Set me up with a date, date coaching", No, Employed for ' \
+                 'wages, Scientist, Master’s degree, Therapy '
+    person_str = 'time,gender,sexuallity,age,income,race,bodyweight,virgin,prostitution_legal,pay_for_sex,friends,' \
+                 'social_fear,depressed,what_help_from_others,attempt_suicide,employment,job_title,edu_level,' \
+                 'improve_yourself_how\n' + person_str
+    # file = open('temp.csv', 'w+')
+    # file.write(unicode(str, errors='replace'))
+    # file.close()
+    target_name = "depressed"
+    temp_person = pd.read_csv('temp.csv', delimiter=",", dtype={target_name: str})
+
+    personVals = []
+    for col in temp_person:
+        if col == 'job_title' or col == 'depressed' or col == 'time' or col == 'social_fear' or col == 'what_help_from_others' or col == 'attempt_suicide' or col == 'improve_yourself_how':
+            continue
+        enum = {target: idx for idx, target in enumerate(sorted(list(set(df[col].values))))}
+        personVals.append(np.vectorize(lambda x: enum[x])(temp_person[col].values))
+
+    personVals = np.array(personVals).T
+    # X = df.drop([target_name], axis=1).values
+
+    # personVals = (personVals - personVals.mean(axis=0)) / personVals.std(axis=0)
+    return personVals
 
 
 if __name__ == '__main__':
@@ -54,7 +79,7 @@ if __name__ == '__main__':
 
     # Read csv data + normalize features
     print("Reading '{}'...".format('data/foreveralone.csv'))
-    X, y, n_classes = parsePeople()
+    X, y, n_classes, df = parsePeople()
     print(" -> X.shape = {}, y.shape = {}, Number of classes = {}\n".format(X.shape, y.shape, n_classes))
     N, d = X.shape
 
@@ -74,6 +99,8 @@ if __name__ == '__main__':
 
     # Train/evaluate the model on each fold
     acc_train, acc_valid = list(), list()
+
+    print()
     print("Cross-validating with {} folds...".format(len(idx_folds)))
     for i, idx_valid in enumerate(idx_folds):
         # Collect training and test data from folds
@@ -85,7 +112,7 @@ if __name__ == '__main__':
         model = Network(input_dim=d, output_dim=n_classes,
                         hidden_layers=hidden_layers, seed=seed_weights)
         model.train(X_train, y_train, eta=eta, n_epochs=n_epochs)
-
+        print(model.predict(parsePerson('sa', df)))
         # Make predictions for training and test data
         ypred_train = model.predict(X_train)
         ypred_valid = model.predict(X_valid)
